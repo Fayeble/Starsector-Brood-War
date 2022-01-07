baa5f57741db0e151caf1276b5935c191be96e5f
Fayeble <35507599+William-Grant-Fayegee@users.noreply.github.com>
2022-01-07 17:55:31 -0500

    Add files via upload

---
+++ data/hullmods/terran_Missiles.java
@@ -0,0 +1,34 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+
+public class terran_Missiles extends BaseHullMod {
+
+	public static final float DAMAGE_REDUCTION = 25f;
+	public static final float MISSILE_MAX_SPEED = 100f;
+	
+	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+		stats.getMissileWeaponDamageMult().modifyMult("terran_Missiles", 1 - DAMAGE_REDUCTION * 0.01f);
+		stats.getMissileMaxSpeedBonus().modifyMult("terran_Missiles", 1 + MISSILE_MAX_SPEED * 0.01f);
+	}
+		
+	public String getDescriptionParam(int index, HullSize hullSize) {
+		if (index == 0) return "" + (int) DAMAGE_REDUCTION + "%";
+		if (index == 1) return "" + (int) MISSILE_MAX_SPEED + "%";
+		return null;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ data/hullmods/terran_Shields.java
@@ -0,0 +1,39 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.combat.ShieldAPI;
+
+public class terran_Shields extends BaseHullMod {
+
+	public static final float SHIELD_BONUS_UNFOLD = 2000f;
+	public static final float UPKEEP_PENALTY = 150f;
+	
+	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+		stats.getShieldUnfoldRateMult().modifyPercent(id, SHIELD_BONUS_UNFOLD);
+		stats.getShieldUpkeepMult().modifyMult(id, 1f + UPKEEP_PENALTY * 0.01f);
+	}
+
+	public String getDescriptionParam(int index, HullSize hullSize) {
+		if (index == 0) return "" + (int) UPKEEP_PENALTY + "%";
+		return null;
+	}
+
+	public boolean isApplicableToShip(ShipAPI ship) {
+		return ship != null && ship.getShield() != null && !ship.getVariant().getHullMods().contains("acceleratedshieldemitter");
+	}
+	
+	public String getUnapplicableReason(ShipAPI ship) {
+		if (ship == null || ship.getShield() == null) {
+			return "Ship has no shields";
+		}
+
+		if (ship.getVariant().getHullMods().contains("acceleratedshieldemitter")) {
+			return "Incompatible with Accelerated Shields";
+		}
+
+		return null;
+	}
+}
